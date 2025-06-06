34f1bd5098f852f358d42386412efe989fe0806a
cf-neto <carlosfdsn2008@gmail.com>
2025-06-06 14:49:13 -0300

    feat: Adding hamburguer icon

--- public/js/utils.js
+++ public/js/utils.js
@@ -14,3 +14,15 @@ function showPasswordToggle(){
 };
 
 showPasswordToggle();
+
+function openMobileMenu(){
+    const mobileMenu = document.getElementById('mobile-menu');
+    const navList = document.getElementById('nav-list');
+
+    mobileMenu.addEventListener('click', () => {
+        navList.classList.toggle('active');
+        mobileMenu.classList.toggle('active');
+    })
+}
+
+document.addEventListener('DOMContentLoaded', openMobileMenu);
