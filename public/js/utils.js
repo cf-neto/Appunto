846b80ddd11f3a47e20a53d764203cd22e672d69
cf-neto <carlosfdsn2008@gmail.com>
2025-06-07 12:55:20 -0300

    fixing js

--- public/js/utils.js
+++ public/js/utils.js
@@ -1,20 +1,3 @@
-function showPasswordToggle(){
-    const input = document.getElementById('password');
-    const iconLock = document.getElementById('icon-lock');
-
-    if (!iconLock || !input) return;
-
-    iconLock.addEventListener('click', () => {
-        const isVisible = input.type === "text";
-        input.type = isVisible ? 'password' : 'text';
-        iconLock.classList.toggle('bxs-lock', isVisible);
-        iconLock.classList.toggle('bxs-lock-open', !isVisible);
-
-    });
-};
-
-showPasswordToggle();
-
 function openMobileMenu(){
     const mobileMenu = document.getElementById('mobile-menu');
     const navList = document.getElementById('nav-list');
